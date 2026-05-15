8736ace42fe3c106eedf1e31840ca96ed7cd5861
Nurlaila UIN MATARAM <250306006.mhs@uinmataram.ac.id>
2026-05-15 21:06:48 +0800

    Add files via upload

---
+++ StrukturDataLinear(Array).py
@@ -0,0 +1,8 @@
+#Array (python list)
+data = [10, 20, 30, 40]
+
+print(data[0])
+
+data.append(50)  #append = ditaruh di belakang
+data.remove(20)
+print(data)
---
+++ StrukturDataLinear(Stack).py
@@ -0,0 +1,9 @@
+#Stack
+stack = []
+
+stack.append(10)
+stack.append(20)
+
+stack.pop()
+
+print(stack)
